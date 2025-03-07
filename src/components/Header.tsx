97fac77f6b2cc469fd2b76f33db4b053be0f2870
PhoenixFan86 <johndoe@example.comphoenixfan86@gmail.com>
2025-03-07 12:23:58 +0200

    change

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -8,7 +8,7 @@ const Header = () => {
     <header className="fixed top-0 left-0 w-full h-[50px] flex items-center z-40 bg-gradient-to-b from-zinc-900 to-zinc-900/0">
       <div className="max-w-screen-2xl w-full mx-auto px-4 flex justify-between items-center md:px-6">
         <h1 className="headline-2">
-          <a href="#">Portfolio page</a>
+          <a href="#">Portfolio</a>
         </h1>
         <div className="relative md:justify-self-center">
           <button className="menu-btn" onClick={() => setNavOpen((prev) => !prev)}>
