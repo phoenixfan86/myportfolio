25a6b9e049c38bb37b965bfe0dd5c47f21ecc3e1
PhoenixFan86 <johndoe@example.comphoenixfan86@gmail.com>
2025-03-07 12:41:59 +0200

    change vite

--- vite.config.ts
+++ vite.config.ts
@@ -5,7 +5,7 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 
 export default defineConfig({
-  base:'/myPortfolio/',
+  base:'/myportfolio/',
   plugins: [react(), tailwindcss(),],
   resolve:{
     alias:{
