91f533ff77680d1f89e3e4ac2250aebb52d48ec0
phoenixfan86 <phoenixfan86@gmail.com>
2025-03-19 20:56:16 +0200

    Update Work.tsx
    Add new project

--- src/components/Work.tsx
+++ src/components/Work.tsx
@@ -31,6 +31,12 @@ const works = [
     title: 'myPortfolio Page',
     tags: ['HTML', 'CSS', 'React', 'TypeScript'],
     projectLink: 'https://phoenixfan86.github.io/myportfolio/'
+  },
+  {
+    imgSrc: './img/project/Pizzahouse_1.jpg',
+    title: 'PizzaHouse',
+    tags: ['HTML', 'TailwindCSS', 'React', 'TypeScript', 'MongoDB'],
+    projectLink: 'https://mypizzahouse.vercel.app/'
   }
 ]
 
