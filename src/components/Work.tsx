cb0992f038d5d11bfd4640620a2f9085ef4da827
PhoenixFan86 <johndoe@example.comphoenixfan86@gmail.com>
2025-10-30 13:20:28 +0200

    add project

--- src/components/Work.tsx
+++ src/components/Work.tsx
@@ -3,6 +3,12 @@ import ProjectCard from './ProjectCard'
 
 const works = [
   {
+    imgSrc: './img/project/ai-moneymaker_1.png',
+    title: 'AI moneymaker',
+    tags: ['React', 'TypeScript', 'CSS'],
+    projectLink: 'https://ai-moneymaker.vercel.app/'
+  },
+  {
     imgSrc: './img/project/coffee_shop.png',
     title: 'Coffee shop app',
     tags: ['React', 'NextJS', 'CSS', 'Markdown'],
